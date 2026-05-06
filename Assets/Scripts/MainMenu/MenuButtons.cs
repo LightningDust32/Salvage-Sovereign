af84bf33be927ff937d2f1604ee245400997a2e0
LightningDust32 <superiorhunter09@gmail.com>
2026-05-06 16:20:33 +0100

    Added credits

--- Assets/Scripts/MainMenu/MenuButtons.cs
+++ Assets/Scripts/MainMenu/MenuButtons.cs
@@ -3,14 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    [SerializeField] GameObject menu;
+    [SerializeField] GameObject credits;
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
     public void GameCredits()
     {
-        // Switch canvas to show names and such
+        credits.SetActive(true);
+        menu.SetActive(false);
     }
+
+    public void Back()
+    {
+        credits.SetActive(false);
+        menu.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
