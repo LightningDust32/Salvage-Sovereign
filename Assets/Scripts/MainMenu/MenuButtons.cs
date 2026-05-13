e3af9155a10872a9d4fd0d29d3414917a1a2b786
BillyB003 <billy.b.sp9@gmail.com>
2026-05-13 16:42:16 +0100

    Got the settings button hooked

--- Assets/Scripts/MainMenu/MenuButtons.cs
+++ Assets/Scripts/MainMenu/MenuButtons.cs
@@ -3,24 +3,42 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    [SerializeField] GameObject menu;
-    [SerializeField] GameObject credits;
+    [SerializeField] GameObject menuScreen;
+    [SerializeField] GameObject creditsScreen;
+    [SerializeField] GameObject settingsScreen;
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
-    public void GameCredits()
+    public void GameCredits(bool credits)
     {
-        credits.SetActive(true);
-        menu.SetActive(false);
+        if (credits != true)
+        {
+            creditsScreen.SetActive(true);
+            menuScreen.SetActive(false);
+        }
+        else
+        {
+            creditsScreen.SetActive(false);
+            menuScreen.SetActive(true);
+        }
+
+    }
+    public void GameSettings(bool settings)
+    {
+        if (settings != true)
+        {
+            settingsScreen.SetActive(true);
+            menuScreen.SetActive(false);
+        }
+        else
+        {
+            settingsScreen.SetActive(false);
+            menuScreen.SetActive(true);
+        }
     }
 
-    public void Back()
-    {
-        credits.SetActive(false);
-        menu.SetActive(true);
-    }
 
     public void QuitGame()
     {
